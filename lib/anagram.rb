8f82a8debcdf471cb659d0a50e96138e2f7f8e14
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-24 17:55:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ def match(array)
  words_sort = array.map { |item| item.chars.sort.join}
  match = @word.chars.sort.join
  if words_sort.include?(match)
-   array.index[words_sort.index(match)]
+   array[words_sort.index(match)]
  end
 
 end
